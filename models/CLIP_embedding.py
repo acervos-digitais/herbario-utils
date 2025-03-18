1f2b27e54999d33e261e8c37db6e4180edee388a
Thiago Hersan <thiago.hersan+github@gmail.com>
2025-03-17 22:33:01 -0400

    simplify clip embedding

--- models/CLIP_embedding.py
+++ models/CLIP_embedding.py
@@ -2,17 +2,16 @@ import torch
 
 from transformers import CLIPProcessor, CLIPModel
 
-from .EmbeddingModel import EmbeddingModel
-
-class Clip(EmbeddingModel):
+class Clip:
   MODEL_NAME = "openai/clip-vit-large-patch14"
+  DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
   def __init__(self):
     self.processor = CLIPProcessor.from_pretrained(Clip.MODEL_NAME)
-    self.model = CLIPModel.from_pretrained(Clip.MODEL_NAME).to(EmbeddingModel.device)
+    self.model = CLIPModel.from_pretrained(Clip.MODEL_NAME).to(Clip.DEVICE)
 
   def get_embedding(self, imgs):
-    inputs = self.processor(images=imgs, return_tensors="pt", padding=True).to(Clip.device)
+    inputs = self.processor(images=imgs, return_tensors="pt", padding=True).to(Clip.DEVICE)
 
     with torch.no_grad():
       my_embedding = self.model.get_image_features(**inputs).detach().squeeze()
