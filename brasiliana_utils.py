214141816fcb3a5ebd9e0f310ec019b517b67f80
Thiago Hersan <thiago.hersan+github@gmail.com>
2025-03-24 19:33:51 -0400

    reorg brasiliana_utils to be like wiki_utils

--- brasiliana_utils.py
+++ brasiliana_utils.py
@@ -1,28 +1,24 @@
 import json
 import re
+import requests
 import urllib.request as request
 
-from models.EnPt import PtEn
+from io import BytesIO
 
-"""
-i["id"]
-i["url"]
-i["document"]["value"]
-"""
+from PIL import Image as PImage, ImageOps as PImageOps
+PImage.MAX_IMAGE_PIXELS = None
+
 
 class Brasiliana:
   API_URL = "https://brasiliana.museus.gov.br/wp-json"
   TAINACAN_URL = f"{API_URL}/tainacan/v2/items"
 
-  # TODO: add categories
-  QUERY_PAINTING_URL = f"{TAINACAN_URL}/?" + \
-    "perpage=100&" + \
-    "order=ASC&orderby=date&" + \
-    "taxquery%5B0%5D%5Btaxonomy%5D=tnc_tax_27&" + \
-    "taxquery%5B0%5D%5Bterms%5D%5B0%5D=1076&" + \
-    "taxquery%5B0%5D%5Bcompare%5D=IN&" + \
-    "exposer=json-flat&" + \
-    "paged=1"
+  CATEGORIES = {
+    "painting": 1076,
+    "drawing": 1069,
+    "print": 1067,
+    "money": 15
+  }
 
   ITEM_DATA_FIELDS = {
     "title": "title",
@@ -46,14 +42,25 @@ class Brasiliana:
       return json.load(in_file)
 
   @classmethod
-  def run_category_query(cls, category):
-    # TODO: add categories
-    if category == "painting":
-      return cls.run_query(cls.QUERY_PAINTING_URL)
+  def prep_category_query(cls, category_label):
+    if category_label not in cls.CATEGORIES:
+      raise Exception("Invalid Category:", category_label)
+
+    return f"{cls.TAINACAN_URL}/?" + \
+      "perpage=100&" + \
+      "order=ASC&orderby=date&" + \
+      "taxquery%5B0%5D%5Btaxonomy%5D=tnc_tax_27&" + \
+      f"taxquery%5B0%5D%5Bterms%5D%5B0%5D={cls.CATEGORIES[category_label]}&" + \
+      "taxquery%5B0%5D%5Bcompare%5D=IN&" + \
+      "exposer=json-flat&" + \
+      "paged=1"
+
+  @classmethod
+  def run_category_query(cls, category_label):
+    return cls.run_query(cls.prep_category_query(category_label))
 
   @classmethod
   def run_query(cls, url):
-    # TODO: get json from online
     result = cls.object_from_json_url(url)
     items = result["items"]
 
@@ -64,13 +71,17 @@ class Brasiliana:
           "en": ""
         }
 
-        if len(i["data"][f]["value"]["pt"]) > 0:
-          i["data"][f]["value"]["en"] = PtEn.translate(i["data"][f]["value"]["pt"])
-
     pagination = result["pagination"]
     if(pagination["current_page"] < pagination["total_page"]):
-      print("get page:", pagination["next_page"])
+      print("page", pagination["next_page"])
       nurl = re.sub(r"paged=[0-9]+", f"paged={pagination['next_page']}", url)
       items += cls.run_query(nurl)
 
     return items
+
+  @classmethod
+  def download_image(cls, img_url):
+    response = requests.get(img_url)
+    response.raise_for_status()
+    pimg = PImage.open(BytesIO(response.content)).convert("RGB")
+    return PImageOps.exif_transpose(pimg)
