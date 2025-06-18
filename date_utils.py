9bcaeca248c1725728f046c047343fd325b12aab
Thiago Hersan <thiago.hersan+github@gmail.com>
2025-06-18 19:15:10 -0300

    fix date parsing

--- date_utils.py
+++ date_utils.py
@@ -17,13 +17,16 @@ C2Y = {
 def get_year(date_str):
   date_str = date_str.strip().lower()
   years = re.findall(r"[1-2][0-9]{3}", date_str)
-  cents = re.findall(r"[1-2][0-9]\-", date_str)
+  cents = re.findall(r"[1-2][0-9][\-\. ]", date_str)
+  decs = re.findall(r"[1-2][0-9]{2}[\-\. ]", date_str)
   roman = re.findall(r"[ivx]{2,8}", date_str)
 
   if len(years) > 0:
     return int(years[0])
   elif len(cents) > 0:
-    return int(cents[0].replace("-", "50"))
+    return int(cents[0][:2] + "50")
+  elif len(decs) > 0:
+    return int(decs[0][:3] + "5")
   elif len(roman) > 0:
     return C2Y[roman[0]]
   elif date_str == "" or any(x in date_str for x in NON_DATE_STRINGS):
